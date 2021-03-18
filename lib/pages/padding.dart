3c6e93d01a329ebfbad8f94ff226568c607d7dde
prabhunath111 <prabhutiwari111@gmail.com>
2021-03-18 15:05:23 +0530

    animated padding  widget

--- lib/pages/padding.dart
+++ lib/pages/padding.dart
@@ -6,6 +6,7 @@ class PaddingDemo extends StatefulWidget {
 }
 
 class _PaddingDemoState extends State<PaddingDemo> {
+  bool paddingSmall = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +14,76 @@ class _PaddingDemoState extends State<PaddingDemo> {
         title: Text(
             'Padding Demo'
         ),
+      ),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.bottomLeft,
+                end: Alignment.topRight,
+                colors: [Colors.deepOrange, Colors.amberAccent]),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Animated Padding Demo',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              ),
+
+              Container(
+                height: 280,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        getAnimPaddingWidget(),
+                        getAnimPaddingWidget(),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        getAnimPaddingWidget(),
+                        getAnimPaddingWidget(),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 15.0),
+              RaisedButton(
+                  color: Colors.brown,
+                  child: Text('Change Padding'),
+                  onPressed: () {
+                    setState(() {
+                      paddingSmall=!paddingSmall;
+                    });
+                  }
+              ),
+              SizedBox(height: 25.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getAnimPaddingWidget() {
+    return AnimatedPadding(
+      curve: Curves.bounceOut,
+      duration: Duration(milliseconds: 500),
+      padding: paddingSmall?EdgeInsets.all(5) : EdgeInsets.all(30),
+      child: Container(
+        height: 80.0,
+        width: 80.0,
+        color: Colors.blue,
       ),
     );
   }
